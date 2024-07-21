f96be5065b915f00107cee3b85c4b45754d14864
PranavGuhanR <pranavguhanr@gmail.com>
2024-07-21 16:26:55 +0530

    Api

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const express = require('express');
 //an instance of express
 const app = express();
 
-// define a route
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//sample in-memory storage for todo items
+let todo=[];
+
+//create a new todo item
+app.post('/todos', (req, res) => {
+  const {title, description} = req.body;
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      description
+    };
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
 })
 
 // start the server
