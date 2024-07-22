347e96c0c220a7763404f3edb100ca4c5367d55c
PranavGuhanR <pranavguhanr@gmail.com>
2024-07-22 21:16:02 +0530

    before frontend

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ mongoose.connect('mongodb://localhost:27017/mern-app')
 })
 
 //creating schema
+const todoSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    description: String
+})
+
+
 const todoModel =mongoose.model('Todo', todoSchema);
 
 //create a new todo item
@@ -35,18 +44,62 @@ app.post('/todos', async(req, res) => {
         res.status(201).json(newTodo);
     }catch(error){
         console.log(error)
-        res.status(500);
+        res.status(500).json({message: error.message});
     }
+
 })
 
 //get all items
 
-app.get('/todos', (req, res) => {
-    res.json(todos);
-})    
+app.get('/todos', async (req, res) => {
+    try{
+        const todos = await todoModel.find();
+        res.json(todos);
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: error.message});
+    }   
+
+})   
+
+//Update a todo item
+app.put('/todos/:id', async (req, res) => {
+    try{
+        const {title, description} = req.body;
+        const id = req.params.id;
+        const updatedTodo = await todoModel.findByIdAndUpdate(
+            id, 
+            {title, description}, 
+            {new: true}
+        )
+
+        if (!updatedTodo){
+            return res.status(404).json({message: 'Todo not found'})
+        }
+        res.json(updatedTodo)
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: error.message});
+    }    
+
+})
+
+//delete a todo item
+app.delete('/todos/:id', async(req, res) => {
+    try{
+        const id = req.params.id;
+        await todoModel.findByIdAndDelete(id);
+        res.status(204).end();
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: error.message});
+    }  
+
+    })
 
 // start the server
 const port=3000;
 app.listen(port, () => {
   console.log('Server started on port:'+port);
+
 })
